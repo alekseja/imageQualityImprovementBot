89ea502542e634aa4a59b0756b745a7556530a99
Aleksey Berko <i@aberko.ru>
2024-08-10 20:05:15 +0300

    start of project

---
+++ src/main/java/ru/aoi15/imagequalityimprovementbot/ImageQualityImprovementBotApplication.java
@@ -0,0 +1,13 @@
+package ru.aoi15.imagequalityimprovementbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageQualityImprovementBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImageQualityImprovementBotApplication.class, args);
+    }
+
+}
